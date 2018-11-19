9f9f7f95ac7f4e624fd3198b42ec95dc3c797c22
Matej <mlipcinski@abv.bg>
2018-11-19 15:24:27 +0200

    add error handling to http requests

--- src/app/pages/admin-panel/users/users.component.ts
+++ src/app/pages/admin-panel/users/users.component.ts
@@ -11,7 +11,7 @@ import { UsersService } from '../../../services/users/users.service';
 export class UsersComponent implements OnInit {
   new: boolean = false;
   user: User = new User();
-  users: User[];  
+  users: any;  
 
   constructor(private userService: UsersService) { }
 
@@ -21,31 +21,39 @@ export class UsersComponent implements OnInit {
 
   private getUsers(): void {
     this.userService.getUsers().subscribe((data: User[]) => {
-      this.users = data;
-    });
+      this.users = data[0] ;
+    }, err => this.showError(err),);
   }
 
   public createUser(user: User) {
     this.userService.insertUser(user).subscribe((data: User) => {
       this.new = false;
       this.getUsers();
-    });
+    }, err => this.showError(err),);
+    
   }
 
   public editUser(user: User) {
     this.userService.updateUser(user).subscribe((data: User) => { 
-      this.getUsers() 
-    });
+      
+    }, err => this.showError(err),
+    );
+    this.getUsers()
   }
 
   public deleteUser(user: User) {
     this.userService.deleteUser(user.id).subscribe((data: User) => {
-       this.getUsers() 
-      });
+      this.getUsers() 
+      }, err => this.showError(err),);
+      
   }
 
-  public newClick() {
+  public newClick(isNew: boolean) {
     this.user = new User();
-    this.new = true;
+    this.new = isNew;
+  }
+
+  private showError(err: any): void {
+    alert(err.error.message);
   }
 }
